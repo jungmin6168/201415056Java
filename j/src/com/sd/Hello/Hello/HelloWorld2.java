4cef4acb03059f56d9977d18caf7a63f121d87e4
jungmin6168 <s.j.min136@gmail.com>
2016-10-16 16:26:05 +0900

    Add files via upload

---
+++ j/src/com/sd/Hello/Hello/HelloWorld2.java
@@ -0,0 +1,14 @@
+package com.sd.hello;
+
+public class HelloWorld2{
+ 
+ public void hello(){
+      System.out.println("Hello World from hello()");
+ }
+  public void hello(String s){
+        System.out.printf("Hello World from hello(string s)"+s);
+  }
+ public static void main(String[] agrs){
+    System.out.println("Hello World");
+ }
+}
