6c194cec92b6f9c8b222075f9d9939f747a8a2f1
jungmin6168 <s.j.min136@gmail.com>
2016-10-26 01:12:50 +0900

    Add files via upload

---
+++ TurtleTriangle.java
@@ -0,0 +1,17 @@
+package com.sd.Turtle;
+import ch.aplu.Turtle.*;
+
+class TurtleTriangle {
+Turtle t1 = new Turtle();
+TurtleTriangle(){
+for(int i=0; i<3; i++)
+{
+t1.forward(100);
+t1.right(120);
+}
+}
+public static void main(String[] args)
+{
+new TurtleTriangle();
+}
+}
