f276dd29963a2ca43f6bf3950e3ca63cd3f96087
jungmin6168 <s.j.min136@gmail.com>
2016-11-21 11:00:10 +0900

    Add files via upload

--- TurtleSquare.java
+++ TurtleSquare.java
@@ -1,20 +1,27 @@
-package com.sd.Turtle;
 import ch.aplu.turtle.*;
+import java.util.ArrayList;
 
 class TurtleSquare {
-Turtle t1 = new Turtle();
-
-TurtleSquare()
-{
-for(int i = 0; i < 4;i++)
-{
-t1.forward(100);
-t1.right(90);
-}
-}
-
-public static void main(String[] args)
-{
-new TurtleSquare();
-}
+	public ArrayList<Double[]> posArr = new ArrayList<Double[]>();
+	Turtle t1 = new Turtle();
+	
+	public void drawSquare() {
+		
+		for (int i = 0; i < 4; i++) {
+			Double[] pos = new Double[2];
+			pos[0] = t1.getX();
+			pos[1] = t1.getY();
+			posArr.add(pos);
+			t1.fd(100);
+			t1.right(90);
+		}
+		
+		for (Double[] d : posArr)
+			System.out.printf("(%.1f, %.1f)", d[0], d[1]);
+	}
+	
+	public static void main(String[] args) {
+		TurtleSquare ts = new TurtleSquare(); 
+		ts.drawSquare();
+	}
 }
