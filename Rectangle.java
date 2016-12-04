e0df4d7270107f60cd1badfb14bfe153908bb76e
jungmin6168 <s.j.min136@gmail.com>
2016-12-05 01:04:28 +0900

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,25 @@
+public class Rectangle implements IShape {
+	private double width = 0;
+	private double height = 0;
+	private double area = 0;
+	private double perimeter = 0;
+
+	public void Rectangle(double w, double h) {
+	this.width = w;
+	this.height = h;
+	this.area = w*h;
+	this.perimeter = 2*(w+h);
+	}
+
+	@Override
+	public double calcArea() {
+	return this.area;
+	}
+	@Override
+	public double calcPerimeter(){
+	return this.perimeter;
+	}
+	public String toString() {
+	return "Rectangle...." + super.toString();
+	} 
+}
